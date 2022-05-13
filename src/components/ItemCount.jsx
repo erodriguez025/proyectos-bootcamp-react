dd2a4fc7475ecd01317ba8e1bf5d9e31521384d2
Juan Rodriguez <emirodriguez_025@hotmail.com>
2022-05-13 16:50:34 -0300

    se incorpora función ItemCount

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function ItemCount() {
+export default function ItemCount( { stock, initial, onAdd} ) {
+    const [contador, setContador] = useState(initial);
+
+    const sumar = ()=> {
+        if (contador < stock){
+          setContador(contador + 1)
+        }
+    }
+
+    const restar = ()=> {
+        if (contador > 0){
+          setContador(contador - 1)
+        }
+    }
+
   return (
-    <div>ItemCount</div>
+    <div>
+        <h4>Libro: Las crónicas de Narnia 📚</h4>
+        <nav>
+          <button onClick={restar} disabled={contador===0} >-</button>
+          <button onClick={sumar} disabled={contador===stock}>+</button>
+        </nav>
+        <h4>{contador}</h4>
+        <button type="button" class="btn btn-primary" data-bs-toggle="button">Agregar al carrito</button>
+    </div>
   )
 }
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import ItemCount from './ItemCount';
 
 export default function ItemListContainer( props) {
-    let resultado = props.greeting
+  let resultado = props.greeting
+
+  const onAdd=(cantidad)=>{
+    alert("Compro: " + cantidad)
+  }
+
+
   return (
-    <div>{resultado}</div>
+    <>
+      <div>{resultado}</div>
+      <ItemCount stock={5} initial={0} onAdd={onAdd} />
+    </>
   )
 }
