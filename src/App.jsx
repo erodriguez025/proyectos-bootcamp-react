4542791282866cdf7433b1224ac79329534963ec
Juan Rodriguez <emirodriguez_025@hotmail.com>
2022-05-13 15:15:30 -0300

    Se incorpora el componente ItemCount.js

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
+import ItemCount from './components/ItemCount';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <NavBar/>
       <ItemListContainer greeting="Acá irían las carts con los libros según la selección del usuario" />
+      <ItemCount/>
     </div>
   );
 }
